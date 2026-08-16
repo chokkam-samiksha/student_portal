fb1376c5eb9b804d92652d8864dd15996df839a5
Chokkam Samiksha <chokkamsamiksha@gmail.com>
2026-08-16 19:29:13 +0530

    Restore student implementation from Git history

---
+++ src/models/student_record.cpp
@@ -0,0 +1,10 @@
+#include "../../include/student.h"
+#include <iostream>
+using namespace std;
+
+void displayStudent(const Student& student) {
+    cout << "\n=== Student Information ===\n";
+    cout << "Name: " << student.name << '\n';
+    cout << "Student ID: " << student.studentId << '\n';
+    cout << "Department: " << student.department << '\n';
+}
